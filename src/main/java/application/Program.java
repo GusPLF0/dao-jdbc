340b601f74daacc769a66894b932da1603545a56
GuPLF0 <gpedr56@gmail.com>
2022-12-24 09:45:02 -0300

    Seller entity class + Using lombok

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,11 +1,30 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-        Department books = new Department(1, "Books");
-        
+        Department books = Department
+                .builder()
+                .id(1)
+                .name("Books")
+                .build();
+
+
+        Seller seller = Seller
+                .builder()
+                .id(21)
+                .name("Bob")
+                .email("testBob@gmail.com")
+                .birthDate(new Date())
+                .baseSalary(3000.0)
+                .department(books)
+                .build();
+
+        System.out.println(seller);
     }
 }
--- src/main/java/model/entities/Seller.java
+++ src/main/java/model/entities/Seller.java
@@ -3,27 +3,32 @@ package model.entities;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
+
 @Builder
-@ToString
 @Getter
 @Setter
-public class Department implements Serializable {
+@ToString
+public class Seller implements Serializable {
 
     private Integer id;
     private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+    private Department department;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Department that = (Department) o;
-        return id.equals(that.id);
+        Seller seller = (Seller) o;
+        return Objects.equals(id, seller.id);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
-
 }
